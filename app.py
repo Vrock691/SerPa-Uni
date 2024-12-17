92848ed271ec194ef7c844196266de0d68fb4945
Valentin Mary <39530176+Vrock691@users.noreply.github.com>
2024-12-17 15:56:23 +0000

    add: Début de l'implémentation de cv2 dans le streamlit

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 import streamlit as st
 import pandas as pd
+import cv2
+import numpy as np
+from PIL import Image
+
  
+image = None 
+
 st.write("""
 # SerPA
+         
+Uploadez une image ici :
 """)
 
-def onImageUploaded():
-    st.write('uploaded')
+image = st.file_uploader("Image", type=['png', 'jpg'] , accept_multiple_files=False, label_visibility="visible")
 
-st.file_uploader("Image", type=['png', 'jpg'] , accept_multiple_files=False, label_visibility="visible", on_change=onImageUploaded())
+if image is not None:
+    st.write("""
+        Votre image
+""")
+    # Enregistrement de l'image en mémoire
+    imageToSave = Image.open(image)
+    imageToSave = imageToSave.save('img.png')
 
+    # Conversion de l'image en niveau de gris
+    img = cv2.imread('img.png', cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Filtre canny
+    img = cv2.Canny(img, 50, 50)
+
+    st.image(img)
